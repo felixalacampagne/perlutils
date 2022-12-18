b0f470b3743d8b9d32506116a51ede03aa165b8f
Chris <chris@felixalacampagne.be>
2022-12-18 19:05:01 +0100

    Keep cmd files in cmdutils - reduces the number of path entries needed.

--- flacmd5.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-if "%UTLDIR%" == "" set UTLDIR=C:\Development\utils
-if "%CMDUTLDIR%" == "" set CMDUTLDIR=%UTLDIR%\cmdutils
-if "%PERLUTLDIR%" == "" set PERLUTLDIR=%UTLDIR%\perlutils
-
-set FFMPEG=%UTLDIR%\ffmpeg\bin\ffmpeg
-perl "%PERLUTLDIR%\flacmd5.pl" %*
