0b539d16bf679368192fee861209d9b5ac62216e
unknown <chris@lofty>
2023-03-05 19:19:28 +0100

    Test for elevated privs

--- testregedit.pl
+++ testregedit.pl
@@ -1,10 +1,11 @@
 use 5.010;  # Available since 2007, so should be safe to use this!!
 use strict;
 use warnings;
-use FindBin;           # This should find the location of this script
+use FindBin;           # This should find the directory of this script
 use lib $FindBin::Bin . '/lib'; # This indicates to look for modules in the script location
 use Data::Dumper;
 use Win32::TieRegistry ( Delimiter=>"/");
+use Win32;
 use File::Spec;
 
 my $GSCRIPTPATH = "";
@@ -36,7 +37,7 @@ if( $GPERLPATH eq "" )
 
 if( $GSCRIPTPATH eq "" )
 {
-   $GSCRIPTPATH =  File::Spec->catdir($FindBin::RealBin, $FindBin::RealScript) ;
+   $GSCRIPTPATH =  File::Spec->catdir($FindBin::Bin, $FindBin::Script) ;
    print "Perl script path: $GSCRIPTPATH\n";
 }
 
@@ -99,43 +100,54 @@ my $shellKey;
 
 }
 
-####### main part of prgram - should be a function so it canbe added to foldermd5 'as-is'
-
-# HKEY_CLASSES_ROOT\Directory\shell\FolderMD5
+sub registerfmd5
+{
 my $shellRootName = "";
 
+   # Apparently this can only be read by an Administrator. It may be that it can only be written by Admin
+   # and the default access mode is read/write. Will need to experiment with that. Anyway it looks like just
+   # running the app and creating the keys if they are not there will not work. It will need to be an option
+   # which must be used in an administrator prompt.
 
-# Apparently this can only be read by an Administrator. It may be that it can only be written by Admin
-# and the default access mode is read/write. Will need to experiment with that. Anyway it looks like just
-# running the app and creating the keys if they are not there will not work. It will need to be an option
-# which must be used in an administrator prompt.
+   # There is some scope here for iterating over a list but note that not all options are the same
+   $shellRootName = "Classes/Directory/shell/";
+   addfmd5toKey($shellRootName, "-l -p");
 
-# There is some scope here for iterating over a list but note that not all options are the same
-$shellRootName = "Classes/Directory/shell/";
-addfmd5toKey($shellRootName, "-l -p");
+   $shellRootName = "Classes/Drive/shell/";
+   addfmd5toKey($shellRootName, "-l -p");
 
-$shellRootName = "Classes/Drive/shell/";
-addfmd5toKey($shellRootName, "-l -p");
+   $shellRootName = "Classes/DVD/shell/";
+   addfmd5toKey($shellRootName, "-c -l -p");
 
-$shellRootName = "Classes/DVD/shell/";
-addfmd5toKey($shellRootName, "-c -l -p");
+   # Previously these were under 'FolderMD5 Update' but now use the same name as for Drive,Directory
+   $shellRootName = "Classes/iTunes.m4v/shell/";
+   addfmd5toKey($shellRootName, "-l -p");
 
-# Previously these were under 'FolderMD5 Update' but now use the same name as for Drive,Directory
-$shellRootName = "Classes/iTunes.m4v/shell/";
-addfmd5toKey($shellRootName, "-l -p");
+   $shellRootName = "Classes/WMP11.AssocFile.MP3/shell/";
+   addfmd5toKey($shellRootName, "-l -p");
 
-$shellRootName = "Classes/WMP11.AssocFile.MP3/shell/";
-addfmd5toKey($shellRootName, "-l -p");
+   $shellRootName = "Classes/WMP.FlacFile/shell/";
+   addfmd5toKey($shellRootName, "-l -p");
 
-$shellRootName = "Classes/WMP.FlacFile/shell/";
-addfmd5toKey($shellRootName, "-l -p");
+   $shellRootName = "Classes/WMP11.AssocFile.MP4/shell/";
+   addfmd5toKey($shellRootName, "-l -p");
 
-$shellRootName = "Classes/WMP11.AssocFile.MP4/shell/";
-addfmd5toKey($shellRootName, "-l -p");
-
-# The other type of key is for the 'Extended' context menu entry, the 'Shift-click' menu,
-# where the force recalculate item appears, ie. 'FolderMD5 Recalculate'
-# This is only added for Drive and Directory (makes no sense for DVD which is assumed to be read only)
-# This requires the '"Extended"="' value adding to the 'FolderMD5 Recalculate' key.
-addfmd5RecalctoKey("Classes/Drive/shell/", "-r -l -p");
-addfmd5RecalctoKey("Classes/Directory/shell/", "-r -l -p");
+   # The other type of key is for the 'Extended' context menu entry, the 'Shift-click' menu,
+   # where the force recalculate item appears, ie. 'FolderMD5 Recalculate'
+   # This is only added for Drive and Directory (makes no sense for DVD which is assumed to be read only)
+   # This requires the '"Extended"="' value adding to the 'FolderMD5 Recalculate' key.
+   addfmd5RecalctoKey("Classes/Drive/shell/", "-r -l -p");
+   addfmd5RecalctoKey("Classes/Directory/shell/", "-r -l -p");
+}
+####### main part of prgram - should be a function so it can be added to foldermd5 'as-is'
+if ( Win32::IsAdminUser != 0 )
+{
+   registerfmd5();
+}
+else
+{
+   # Could use Win32::RunAsAdmin (https://metacpan.org/pod/Win32%3a%3aRunAsAdmin) which
+   # restarts the script in a Admin console, which closes immediately which sort of ugly.
+   # No doubt workaround is possible but for now I'll keep it simple
+   print "The (re-)register option is only available when running as Administrator\n";
+}
